fdfe0add3859e06244cd00690a41a30e891e3470
salomonmoreno <salomonmoreno@sqdm.com>
2020-08-25 16:43:57 -0500

    Clase sobre JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges From React!</h1>;
+// const element = React.createElement(
+//     'a',
+//     { href: 'http://platzi.com'},
+//     'Ir a Platzi'
+// )
+
+// const name = 'Luisa';
+// const sum = () => 3+3;
+// const element = React.createElement('h1',{}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola, soy {name}</h1>
+
+const jsx = <div>
+    <h1>Hola, soy Salomon</h1>
+    <p>Soy ingeniero Front-End</p>
+</div>
+
+// const element = React.createElement(
+//     'div', 
+//     {},
+//     React.createElement('h1', {}, 'Hola, soy Salomon'),
+//     React.createElement('p', {}, 'Soy Ingeniero FrontEnd')
+// );
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
