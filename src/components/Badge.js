c14b2034d85c6b1316234eacccbf40ff9f9ad415
salomonmoreno <salomonmoreno@sqdm.com>
2020-08-26 15:13:02 -0500

    Primeros Estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
-import confLogo from '../images/badge-header.svg'
+import './styles/Badge.css';
+import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
     render() {
-        return <div>
-            <div>
+        return <div className='Badge'>
+            <div className='Badge_header'>
                 <img src={confLogo} alt="Logo Conferencia" />
             </div>
 
-            <div>
-                <img src="https://www.gravatar.com/avatar?id=identicon" alt="Avatar" />
+            <div className='Badge_section-name'>
+                <img className='Badge_avatar' src="https://s.gravatar.com/avatar/a8a7b2676b97e2c80237b810fa01f4e2?s=80" alt="Avatar" />
                 <h1>Salomon <br /> Moreno</h1>
             </div>
 
-            <div>
-                <p>FrontEnd Engineer</p>
-                <p>@smorenoz</p>
+            <div className='Badge_section-info'>
+                <h3>FrontEnd Engineer</h3>
+                <div>@smorenoz</div>
             </div>
 
-            <div>
-                #platziconfig
+            <div className='Badge_footer'>
+                #platziconf
             </div>
         </div>
     }
