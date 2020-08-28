3de4c982f1ec4030f870d64772ceada65f2b8aa5
salomonmoreno <salomonmoreno@sqdm.com>
2020-08-27 21:31:40 -0500

    Entiendo el setState y state

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-    handleChange(e) {
-        console.log({ value: e.target.value, name: e.target.name })
+    state = {
+        JobTitle: 'Developer'
+    };
+    handleChange = e => {
+        // console.log({ value: e.target.value, name: e.target.name })
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     // Es equivalente a declarar el metodo
@@ -10,13 +16,14 @@ class BadgeForm extends React.Component {
     //     console.log({value: e.target.value, name: e.target.name})
     // }
 
-    handleClick(e) {
+    handleClick = e => {
         console.log('Botton was clicked')
     }
 
-    handleSubmitt(e) {
+    handleSubmitt = e => {
         e.preventDefault();
         console.log('Form was submitted');
+        console.log(this.state)
     }
 
     render() {
@@ -32,6 +39,51 @@ class BadgeForm extends React.Component {
                             className='form-control'
                             type="text"
                             name='FirstName'
+                            value={this.state.FirstName}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input
+                            onChange={this.handleChange}
+                            className='form-control'
+                            type="text"
+                            name='LastName'
+                            value={this.state.LastName}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            onChange={this.handleChange}
+                            className='form-control'
+                            type="email"
+                            name='Email'
+                            value={this.state.Email}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input
+                            onChange={this.handleChange}
+                            className='form-control'
+                            type="text"
+                            name='JobTitle'
+                            value={this.state.JobTitle}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Social Media</label>
+                        <input
+                            onChange={this.handleChange}
+                            className='form-control'
+                            type="text"
+                            name='SocialMedia'
+                            value={this.state.SocialMedia}
                         />
                     </div>
 
