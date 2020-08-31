cbc3cf0116ad7516caf465f7d7ec05fe2c830aa8
salomonmoreno <salomonmoreno@sqdm.com>
2020-08-31 17:14:23 -0500

    Realizando Peticiones a una Fake API

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Badge from './Badge'
+import {Link} from 'react-router-dom'
 
 class BadgesList extends React.Component {
 
     render() {
+        
+        if(this.props.badges.length === 0){
+            return(
+                <div>
+                    <h4>We did not found any badge</h4>
+                    <Link className='btn btn-primary' to='/badges/new'>Create New Badge</Link>
+                </div>
+            )
+        }
+
         return (
             <ul className='list-unstyled'>
                 {this.props.badges.map(element => {
