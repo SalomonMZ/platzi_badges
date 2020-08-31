ac51a5ba645d350cffe7fb04fd7c7c144e4fe2f4
salomonmoreno <salomonmoreno@sqdm.com>
2020-08-31 14:27:40 -0500

    Not Found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function NotFound(){
-    return <h1>404: Not Found</h1>
+import './styles/NotFound.css'
+
+function NotFound() {
+    return (
+        <>
+            <div className='NotFound__background'>
+                <h1>404 <br /> Not Found</h1>
+            </div>
+        </>
+    )
 }
 
 export default NotFound;
