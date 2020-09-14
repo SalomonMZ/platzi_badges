0ab39265eb1051f69fb53ab45d2ae9bf8285a10f
salomonmoreno <salomonmoreno@sqdm.com>
2020-09-14 09:52:55 -0500

    Cambios minimos en estilos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,15 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <div className='Home__container'>
-                    <div className='row'>
-                        <div className="col-6">
-                            <StartNow/>
-                        </div>
-                        
-                        <div className="col-6">
-                            <img className='Home__img' src={LogoImage} alt="Logo"/>
+                <div className='Home__content'>
+                    <div className='container-fluid'>
+                        <div className='row align-items-center'>
+                            <div className='col-12 col-sm-6 Home__content-left'>
+                                <StartNow />
+                            </div>
+                            <div className="col-12 col-sm-6 Home__content-right">
+                                <img className='Home__img' src={LogoImage} alt="Logo" />
+                            </div>
                         </div>
                     </div>
                 </div>
